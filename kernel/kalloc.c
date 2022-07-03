c2a4be2804a891cdcf71310e876c312e1df678d3
maru8181 <stop_1_supasupa@yahoo.co.jp>
2022-07-03 17:41:22 +0900

    reading アロケート処理

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -26,17 +26,17 @@ struct {
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
-  freerange(end, (void*)PHYSTOP);
+	initlock(&kmem.lock, "kmem");
+	freerange(end, (void*)PHYSTOP);
 }
 
 void
 freerange(void *pa_start, void *pa_end)
 {
-  char *p;
-  p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
-    kfree(p);
+	char *p;
+	p = (char*)PGROUNDUP((uint64)pa_start);
+	for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+		kfree(p);
 }
 
 // Free the page of physical memory pointed at by v,
@@ -46,20 +46,20 @@ freerange(void *pa_start, void *pa_end)
 void
 kfree(void *pa)
 {
-  struct run *r;
+	struct run *r;
 
-  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
-    panic("kfree");
+	if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+		panic("kfree");
 
-  // Fill with junk to catch dangling refs.
-  memset(pa, 1, PGSIZE);
+	// Fill with junk to catch dangling refs.
+	memset(pa, 1, PGSIZE);
 
-  r = (struct run*)pa;
+	r = (struct run*)pa;
 
-  acquire(&kmem.lock);
+	acquire(&kmem.lock);
   r->next = kmem.freelist;
   kmem.freelist = r;
-  release(&kmem.lock);
+	release(&kmem.lock);
 }
 
 // Allocate one 4096-byte page of physical memory.
