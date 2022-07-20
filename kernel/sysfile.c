3e439042eca106cb5e366525f6cea71074a7bdcb
maru8181 <stop_1_supasupa@yahoo.co.jp>
2022-07-20 21:30:26 +0900

    reading syscall

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -21,18 +21,18 @@
 static int
 argfd(int n, int *pfd, struct file **pf)
 {
-  int fd;
-  struct file *f;
+	int fd;
+	struct file *f;
 
-  if(argint(n, &fd) < 0)
-    return -1;
-  if(fd < 0 || fd >= NOFILE || (f=myproc()->ofile[fd]) == 0)
-    return -1;
-  if(pfd)
-    *pfd = fd;
-  if(pf)
-    *pf = f;
-  return 0;
+	if(argint(n, &fd) < 0)
+		return -1;
+	if(fd < 0 || fd >= NOFILE || (f=myproc()->ofile[fd]) == 0)
+		return -1;
+	if(pfd)
+		*pfd = fd;
+	if(pf)
+		*pf = f;
+	return 0;
 }
 
 // Allocate a file descriptor for the given file.
