6f1524c46be65b608aa3e749868b528fdb29479b
maru8181 <stop_1_supasupa@yahoo.co.jp>
2022-08-14 12:19:06 +0900

    reading create

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -241,46 +241,46 @@ bad:
 static struct inode*
 create(char *path, short type, short major, short minor)
 {
-  struct inode *ip, *dp;
-  char name[DIRSIZ];
+	struct inode *ip, *dp;
+	char name[DIRSIZ];
 
-  if((dp = nameiparent(path, name)) == 0)
-    return 0;
+	if((dp = nameiparent(path, name)) == 0)
+		return 0;
 
-  ilock(dp);
+	ilock(dp);
 
-  if((ip = dirlookup(dp, name, 0)) != 0){
-    iunlockput(dp);
-    ilock(ip);
-    if(type == T_FILE && (ip->type == T_FILE || ip->type == T_DEVICE))
-      return ip;
-    iunlockput(ip);
-    return 0;
-  }
+	if((ip = dirlookup(dp, name, 0)) != 0){
+		iunlockput(dp);
+		ilock(ip);
+		if(type == T_FILE && (ip->type == T_FILE || ip->type == T_DEVICE))
+			return ip;
+		iunlockput(ip);
+		return 0;
+	}
 
-  if((ip = ialloc(dp->dev, type)) == 0)
-    panic("create: ialloc");
+	if((ip = ialloc(dp->dev, type)) == 0)
+		panic("create: ialloc");
 
-  ilock(ip);
-  ip->major = major;
-  ip->minor = minor;
-  ip->nlink = 1;
-  iupdate(ip);
+	ilock(ip);
+	ip->major = major;
+	ip->minor = minor;
+	ip->nlink = 1;
+	iupdate(ip);
 
-  if(type == T_DIR){  // Create . and .. entries.
-    dp->nlink++;  // for ".."
-    iupdate(dp);
-    // No ip->nlink++ for ".": avoid cyclic ref count.
-    if(dirlink(ip, ".", ip->inum) < 0 || dirlink(ip, "..", dp->inum) < 0)
-      panic("create dots");
-  }
+	if(type == T_DIR){  // Create . and .. entries.
+		dp->nlink++;  // for ".."
+		iupdate(dp);
+		// No ip->nlink++ for ".": avoid cyclic ref count.
+		if(dirlink(ip, ".", ip->inum) < 0 || dirlink(ip, "..", dp->inum) < 0)
+			panic("create dots");
+	}
 
-  if(dirlink(dp, name, ip->inum) < 0)
-    panic("create: dirlink");
+	if(dirlink(dp, name, ip->inum) < 0)
+		panic("create: dirlink");
 
-  iunlockput(dp);
+	iunlockput(dp);
 
-  return ip;
+	return ip;
 }
 
 uint64
